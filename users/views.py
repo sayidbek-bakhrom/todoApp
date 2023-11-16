90123647e7ccee90c4acd275e1dc1e739f8d8bf1
Sayidbek <sayidbekbakhrom@gmail.com>
2023-11-16 17:19:07 +0500

    update some files

--- users/views.py
+++ users/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login
 
 
 class UserRegisterView(generic.CreateView):
     form_class = UserCreationForm
     template_name = 'signup.html'
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        valid = super(UserRegisterView, self).form_valid(form)
+        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return valid
